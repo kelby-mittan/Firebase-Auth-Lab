c2ec92660f3b6331596e8548925da310a2aac063
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-28 17:23:11 -0500

    Initial Commit

---
+++ Firebase-Auth-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Auth-Lab
+//
+//  Created by Kelby Mittan on 2/28/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase-Auth-LabTests/Firebase_Auth_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Firebase_Auth_LabTests.swift
+//  Firebase-Auth-LabTests
+//
+//  Created by Kelby Mittan on 2/28/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import XCTest
+@testable import Firebase_Auth_Lab
+
+class Firebase_Auth_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
