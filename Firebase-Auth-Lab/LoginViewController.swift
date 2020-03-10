9dd065270b4089f112f315667f7d00a45df558c6
Kelby Mittan <kelbymittan@pursuit.org>
2020-03-10 14:34:17 -0400

    working on login and profile vc

--- Firebase-Auth-Lab/LoginViewController.swift
+++ Firebase-Auth-Lab/LoginViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum AccountState {
+    case existingUser
+    case newUser
 }
 
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    private var accountState: AccountState = .existingUser
+    
+    private var authSession = AuthenticationSession()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextField.text, !email.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            print("missing info")
+            return
+        }
+        
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+    }
+}
+
+
+
+extension UIViewController {
+    func showAlert(title: String, message: String, completion: ((UIAlertAction) -> Void)? = nil) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: completion)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
